12baff01e309021506e65d11b7e2658d3228e169
Ludovic Saufnay <sfnldv@forem.be>
2019-09-03 20:27:13 +0200

    work continue

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+  theme: {
+      primary: '#B71C1C',
+      secondary: '#1A237E',
+      accent: '#82B1FF',
+      error: '#FF5252',
+      info: '#2196F3',
+      success: '#4CAF50',
+      warning: '#FFC107'
+  },
+});
+
